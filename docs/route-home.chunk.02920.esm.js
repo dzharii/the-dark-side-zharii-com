b6f9c706cbd9b901698be92cbf677db867ef3b74
Dmytro Zharii <dima@zharii.com>
2021-08-16 22:22:10 -0700

    initial commit2

--- docs/route-home.chunk.02920.esm.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{FDtd:function(e,o,n){"use strict";n.r(o);var s=n("hosL"),t="home__5d1wS";o.default=()=>Object(s.h)("div",{class:t},Object(s.h)("h1",null,"Home"),Object(s.h)("p",null,"This is the Home component."))}}]);
-//# sourceMappingURL=route-home.chunk.02920.esm.js.map
